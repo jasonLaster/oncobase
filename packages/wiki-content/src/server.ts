c710ba1fd76dc52cb857e09fd09add9b7f018fe9
Jason Laster <jason.laster.11@gmail.com>
2026-05-14 01:51:47 -0700

    Run Vite preview functions on Bun
    (cherry picked from commit 870b746f822b15de3c155cad4254081b6a34e642)

--- packages/wiki-content/src/server.ts
+++ packages/wiki-content/src/server.ts
@@ -8,8 +8,8 @@ import type {
   WikiPageRecord,
   WikiScope,
   WikiSessionIdentity,
-} from "./index.js";
-import { isHiddenFileTreeAssetPath, isHiddenFileTreePath } from "./index.js";
+} from "./index.ts";
+import { isHiddenFileTreeAssetPath, isHiddenFileTreePath } from "./index.ts";
 
 const PUBLIC_CACHE_CONTROL =
   "public, max-age=60, s-maxage=300, stale-while-revalidate=3600";
